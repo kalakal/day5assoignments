639f97b73a06b20e6809769967144fcc85262967
kalakal <sayedfaizanqadri@gmail.com>
2022-11-01 13:07:05 +0530

    remainder program is added

--- Program.cs
+++ Program.cs
@@ -1,41 +1,23 @@
 ﻿using System;
 
 namespace Day5Assign
-{
+{ 
 
-    public class Exercise8
+    class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            int num1, num2, num3;
-            Console.Write("\n\n");
-            Console.Write("Find the largest of three numbers:\n");
-            Console.Write("------------------------------------");
-            Console.Write("\n\n");
 
-            Console.Write("Input the 1st number :");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the  2nd number :");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the 3rd  number :");
-            num3 = Convert.ToInt32(Console.ReadLine());
+            int dividend = 100, divisor = 16;
 
-            if (num1 > num2)
-            {
-                if (num1 > num3)
-                {
-                    Console.Write("The 1st Number is the greatest among three. \n\n");
-                }
-                else
-                {
-                    Console.Write("The 3rd Number is the greatest among three. \n\n");
-                }
-            }
-            else if (num2 > num3)
-                Console.Write("The 2nd Number is the greatest among three \n\n");
-            else
-                Console.Write("The 3rd Number is the greatest among three \n\n");
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
         }
-
     }
 }
+
