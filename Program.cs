c7f73de60fdc1bc063058a4fd8057b349c46a29c
kalakal <sayedfaizanqadri@gmail.com>
2022-11-01 12:52:58 +0530

    Day 5 assignment submitted

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Day5Assign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variable
+            int n;
+
+            //take input 
+            Console.Write("Enter the number = ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            //check if n is even or odd 
+            if (n % 2 == 0)
+                Console.WriteLine(n + " is even");
+            else
+                Console.WriteLine(n + " is odd");
+
+            // wait for user to press any key
+            Console.ReadKey();
+        }
+    }
+}
+
